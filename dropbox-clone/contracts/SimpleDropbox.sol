06656c8fa84c36df3a454cfdbb98646ba37bc33d
isparo <josueparra2892@gmail.com>
2024-05-31 13:16:39 -0600

    adding simple dropbox example

--- dropbox-clone/contracts/SimpleDropbox.sol
+++ dropbox-clone/contracts/SimpleDropbox.sol
@@ -1,3 +1,66 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract SimpleDropbox {
+    string public name = 'SimpleDropbox';
+    uint public fileCount = 0;
+    mapping (uint => File) public files;
+
+    constructor() public {}
+
+    struct File {
+        uint fileId;
+        string fileHash;
+        uint fileSize;
+        string fileType;
+        string fileName;
+        string fileDescription;
+        uint uploadTime;
+        address payable uploader;
+    }
+
+    event FileUploaded(
+        uint fileId,
+        string fileHash,
+        uint fileSize,
+        string fileType,
+        string fileName, 
+        string fileDescription,
+        uint uploadTime,
+        address payable uploader
+    );
+
+    function getFileCount() public view returns (uint) {
+        return fileCount;
+    }
+
+    function uploadFile(
+        string memory _fileHash, 
+        uint _fileSize, 
+        string memory _fileType, 
+        string memory _fileName, 
+        string memory _fileDescription) public {
+
+        // validate incomming data
+        require(bytes(_fileHash).length > 0);
+        require(bytes(_fileType).length > 0);
+        require(bytes(_fileDescription).length > 0);
+        require(bytes(_fileName).length > 0);
+        require(msg.sender!=address(0));
+        require(_fileSize>0);
+
+        fileCount++;
+        files[fileCount] = File(fileCount, 
+             _fileHash,
+             _fileSize,
+             _fileType,
+             _fileName,
+             _fileDescription,
+             block.timestamp,
+             payable(msg.sender)
+        );
+
+        emit FileUploaded(fileCount, _fileHash, _fileSize, _fileType, _fileName, _fileDescription, block.timestamp, payable(msg.sender));
+    }
+    
+}
